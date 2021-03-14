2b21c9840804401ef304fb8566f63979748d63ae
Carlos Guerra <carlos.vlz@outlook.com>
2021-03-14 01:39:15 -0600

    Role file/structure added

---
+++ sauce_demo/data/Roles.js
@@ -0,0 +1,11 @@
+import { Role, Selector } from 'testcafe';
+
+const adminRole = Role(async t => {});
+
+const standardUserRole = Role(async t => {});
+
+const locked_out_user = Role(async t => {});
+
+const problem_user = Role(async t => {});
+
+const performance_glitch_user = Role(async t => {});
